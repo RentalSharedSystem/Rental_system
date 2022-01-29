5b55f13d814b9d0a1387d60cea26e30bdb9dfec7
shubham kumar <rentalshared26@gmail.com>
2022-01-29 13:09:06 +0530

    Authentication

---
+++ config/passport-local.js
@@ -0,0 +1,65 @@
+const passport= require('passport');
+const LocalStratgy=require('passport-local').Strategy;
+const User=require('../models/user');
+passport.use(new LocalStratgy({
+    usernameField:'email',
+    passReqToCallback:true
+},
+ function(req,email,password,done){
+    // find a user and establish identity.
+    User.findOne({email:email},function(err,user)
+    {
+        if(err)
+        {
+            req.flash('error',err);
+            return done(err);
+        }
+        else
+        {
+            if(!user||user.password!=password)
+            {
+                return done(null,false);
+            }
+            return done(null,user);
+        }
+        
+    });
+ }
+ ));
+
+ //serialising the user, deciding the which field to keep in the cookies.
+passport.serializeUser(function(user,done){
+    done(null,user.id);
+})
+// desearialising the key to get the user.
+passport.deserializeUser(function(id,done){
+    User.findById(id,function(err,user){
+        if(err)
+        {
+            console.log('error while finding user while desearializing');
+            return done(err);
+        }
+        else
+        {
+            return done(null,user);
+        }
+    })
+})
+
+passport.checkAuthentication=function(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    else
+    {
+        res.redirect('/users/sign-in');
+    }
+}
+passport.setAuthenticatedUser=function(req,res,next)
+{  
+    if(req.isAuthenticated()){
+        res.locals.user=req.user;
+    }
+    next();
+}
+module.exports=passport;
--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const db=require('./config/mongoose');
-const port=process.env.PORT || 3000;
 const app = express();
-
+const port=process.env.PORT || 3000;
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-stratagies');
+const MongoStore = require('connect-mongo');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('./assets'));
 app.set("view engine","ejs");
 app.set('views','./views');
+app.use(session({
+  name:'Rental_system',
+  secret:'jchdhdcsjhndksh',
+  saveUninitialized:false,
+  resave:false,
+  cookie:{
+      maxAge:(1000*60*100)
+  },
+  store: MongoStore.create({
+      mongoUrl:'mongodb://localhost/Rental_system',
+          autoRemove:'disabled',
+  },(err)=>{
+      console.log(err);
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
 app.use('/',require('./routes/index.js'));
 app.listen(port,function(err){
   if(err)
