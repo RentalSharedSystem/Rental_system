2cff796e9d0106f6fa6097375e4acf8b9462b97a
shubham kumar <rentalshared26@gmail.com>
2022-01-29 13:47:42 +0530

    user sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.signUp=function(req,res)
 {
      return res.render('sign_up');
@@ -5,4 +6,28 @@ module.exports.signUp=function(req,res)
 module.exports.signIn=function(req,res)
 {    
      return res.render('sign_in');
+}
+module.exports.create=function(req,res){
+     User.findOne({email:req.body.email},(err,user)=>{
+          if(err)
+          {
+               console.log(err);
+               return;
+          }
+         if(user)
+         {
+              console.log('user allready there');
+              return res.redirect('/users/sign-in');
+         }
+         else
+         {
+             User.create({
+               email:req.body.email,
+               password:req.body.password,
+               name:req.body.username,
+               pincode:req.body.Pincode,
+               phone:req.body.phone});
+               return res.redirect('/users/sign-in');
+         }
+     });
 }
--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const usersController=require('../controllers/users_controller');
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
+router.post('/sign-up',usersController.create);
 
 module.exports=router;
