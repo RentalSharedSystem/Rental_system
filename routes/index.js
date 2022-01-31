8037b40bfe50f796561fcfba878b3e42c8de574d
shubham kumar <rentalshared26@gmail.com>
2022-01-31 14:52:43 +0530

    user authentication

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ const express= require('express');
 const router=express.Router();
 console.log("router loaded sucessfully");
 const homeController=require('../controllers/home_controllers');
-const usersController=require('../controllers/users_controller');
 router.get('/',homeController.home);
-router.get('/logout',usersController.logout);
 router.use('/users',require('./user.js'));
-
 module.exports=router;
